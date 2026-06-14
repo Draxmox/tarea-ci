9c74dbd16bcbeff3112c8d250a9bd1b480c183cc
Alejandro Camino <168216520+Draxmox@users.noreply.github.com>
2026-06-13 22:17:01 -0500

    Initial commit con CI/CD y Docker

---
+++ src/main/java/com/udla/tarea_ci/TareaCiApplication.java
@@ -0,0 +1,13 @@
+package com.udla.tarea_ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udla/tarea_ci/TareaCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udla.tarea_ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
